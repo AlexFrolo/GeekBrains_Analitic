5980089dfc03454b4fe7f77211f8343ebc5e19e5
Фролов Александр <frolov.mailbox@mail.ru>
2024-10-16 17:55:02 +0300

    first python trainings

---
+++ 001.py
@@ -0,0 +1,16 @@
+'''str = input ("Enter your name...\n")
+print ("Hello", str)
+str1 = input ("How old are you..\n")
+age = int (str1)
+if age > 20 and age < 30:
+    print ("You are so young")
+elif age >30 and age <50:
+    print ("You are wise man")
+else:
+    print ("You are cool anyway")'''
+
+lst1 = list ()
+lst1.insert (0, "First") 
+#print (lst1)
+lst1.append ("Second")
+print (lst1)
